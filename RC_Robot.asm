6c396d5eb8924743a4ad7f6144644b67e77d3c1a
waheed1999 <96083010+waheed1999@users.noreply.github.com>
2021-12-31 20:37:46 +0200

    Update RC_Robot.asm

--- RC_Robot.asm
+++ RC_Robot.asm
@@ -1,14 +1,18 @@
- 
-
- .INCLUDE "UART.INC"
+.INCLUDE "UART.INC"
  .INCLUDE "EXTRNAL_INT.INC"
 
-
+ // VECTOR table
 
  .ORG 0X00
- RJMP MAIN // rest
- RJMP INT0_HANDL // ex0_int
- .ORG 0X60
+ JMP MAIN // Rest code  
+ JMP INT0_HANDL // ex0_int
+
+  .ORG 0X1A     // The address of Uart recive int handeller 
+  jmp RXC_COMP // jump to handeller function
+
+ // Gurde erea 
+
+ .ORG 0X66 // Start of Internal SRAM
  
  
  
@@ -19,7 +23,7 @@ LDI R16,LOW(RAMEND)
 OUT SPL,R16
 LDI R16,HIGH(RAMEND)
 OUT SPH,R16
-//_______________________________________________________
+//___________________
 
 
 
@@ -28,17 +32,26 @@ OUT SPH,R16
 VOID_SET:
 
 
-RCALL UART_START // Intializing UART 
+CALL UART_START // Intializing UART 
 
 
 
-RCALL INT0_EN // Enable INT0 PIN
+CALL INT0_EN // Enable INT0 PIN
 
+LDI R18,0X0F
+IN R17,PINB // Read the value of PINB
+OR R18,R17
+OUT DDRB,R18 // PINB0,PINB1,PINB2 AND PINB3 OUTPUT WITHOUT CHANGE OTHER PINS
+
+
+LDI R18,0XF0
+IN R17,PINB // Read the value of PINB
+AND R18,R17
+OUT PORTB,R18 // PINB0,PINB1,PINB2 AND PINB3 IS LOW WITHOUT CHANGE OTHER PINS
 
 VOID_LOAP :
  
+ NOP
 
-
-
-RJMP VOID_LOAP
+JMP VOID_LOAP
 
