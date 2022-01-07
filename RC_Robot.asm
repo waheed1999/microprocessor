c8b4bb4a76c5ca6a41c105ac5a5f4f874565f1e6
hady AbuBaker Abd-ElAlime <96624424+Hady1234@users.noreply.github.com>
2022-01-07 18:56:49 +0200

    Update RC_Robot.asm

--- RC_Robot.asm
+++ RC_Robot.asm
@@ -1,19 +1,19 @@
-.INCLUDE "UART.INC"
- .INCLUDE "EXTRNAL_INT.INC"
-  .INCLUDE "TIMER.INC"
- .INCLUDE "Ultrasonic.INC"
+ 
 
+ .INCLUDE "UART.INC"
+ .INCLUDE "EXTRNAL_INT.INC"
+ .INCLUDE "TIMER.INC"
+ .INCLUDE "Ultrasonic.INC"
  // VECTOR table
 
  .ORG 0X00
- 
  JMP MAIN // Rest code  
  JMP INT0_HANDL // ex0_int
 
   .ORG 0X1A     // The address of Uart recive int handeller 
   jmp RXC_COMP // jump to handeller function
 
- // Guard area
+ // Gurde erea 
 
  .ORG 0X66 // Start of Internal SRAM
  
@@ -21,12 +21,12 @@
  
  
  MAIN:
- // set up stack pointer 
+ // setup stack pointer 
 LDI R16,LOW(RAMEND)
 OUT SPL,R16
 LDI R16,HIGH(RAMEND)
 OUT SPH,R16
-//___________________
+//_______________________________________________________
 
 
 
@@ -37,11 +37,22 @@ VOID_SET:
 
 CALL UART_START // Intializing UART 
 
+
+
 CALL INT0_EN // Enable INT0 PIN
 CALL Timer0_config 
 CALL ultrasonic_config
+CALL Ultrasonic_GET
 LDI R18,0X0F
 IN R17,PINB // Read the value of PINB
+OR R18,R17
+OUT DDRB,R18 // PINB0,PINB1,PINB2 AND PINB3 OUTPUT WITHOUT CHANGE OTHER PINS
+
+
+LDI R18,0XF0
+IN R17,PINB // Read the value of PINB
+AND R18,R17
+OUT PORTB,R18 // PINB0,PINB1,PINB2 AND PINB3 IS LOW WITHOUT CHANGE OTHER PINS
 
 VOID_LOAP :
  
